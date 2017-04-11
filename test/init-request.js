07e369d07a79dbbf647f6152aa10d66959f32a7e
Evan Shortiss <evanshortiss@gmail.com>
2017-04-10 18:07:18 -0700

    Ensure rhmap specific headers are included with requests

--- test/init-request.js
+++ test/init-request.js
@@ -2,17 +2,28 @@
 
 var sinon = require('sinon');
 var expect = require('chai').expect;
+var proxyquire = require('proxyquire').noCallThru();
 
 describe('init-request', function () {
 
   it('should initialise a request', function () {
-    var genInitFn = require('../lib/init-request');
+    var genInitFn = proxyquire('../lib/init-request', {
+      'fh-instance-url': {
+        getServiceCallHeaders: sinon.stub().returns({
+          'x-request-with': 'with',
+          'x-fh-auth-app': 'auth'
+        })
+      }
+    });
     var url = 'http://localhost:8002';
     var req = {
       __fhOriginalinit: sinon.spy()
     };
     var options = {
-      uri: '/users/12345'
+      uri: '/users/12345',
+      headers: {
+        'x-test': 'testing'
+      }
     };
 
     var initFn = genInitFn(req, options);
@@ -21,7 +32,17 @@ describe('init-request', function () {
     initFn(url);
 
     expect(req.uri).to.equal('http://localhost:8002/users/12345');
+
     expect(req.__fhOriginalinit.calledOnce).to.be.true;
+    console.log(req.__fhOriginalinit.getCall(0).args[0]);
+    expect(req.__fhOriginalinit.calledWith({
+      uri: 'http://localhost:8002/users/12345',
+      headers: {
+        'x-test': 'testing',
+        'x-request-with': 'with',
+        'x-fh-auth-app': 'auth'
+      }
+    })).to.be.true;
   });
 
 });
